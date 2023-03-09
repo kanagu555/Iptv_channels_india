0a6485a90251b8b33c49438a0c653d46374209c2
Kanagaraj K <Kanagarajwhb@gmail.com>
2023-03-09 21:54:36 +0530

    added logo and title

--- src/components/HeaderNavigation/Header.js
+++ src/components/HeaderNavigation/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Header.css";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
 
 function Header() {
   return (
     <div className="header-bar">
-      <div>Logo/ICON</div>
+      <div className="icon-name-con">
+        <LiveTvIcon fontSize="large" />
+        <p className="home-page-name">Free IPTV</p>
+      </div>
       <div className="filter-type">
         <div className="filter-lang">Language</div>
         <div className="filter-coun">Country</div>
